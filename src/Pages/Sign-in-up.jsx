015362e4e90fdea3da98f667ba06d44c22c276d3
Badm1 <shattilov@mail.ru>
2021-03-13 18:13:17 +0300

    Страница на которой будут выводиться формы регистрации и авторизации

---
+++ src/Pages/Sign-in-up.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { RegistrationForm } from '../Components/RegistrationForm'
+import '../CssFiles/Sign-in-up.css'
+
+export const SignPage = () => {
+
+    return (
+        <RegistrationForm />
+    )
+}
