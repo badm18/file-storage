611d4ee3b75db19aeea8b1a1620d02d7de25c2d0
Badm1 <shattilov@mail.ru>
2021-03-14 21:28:01 +0300

    добавлен переход по страницам

--- src/Pages/Sign-in-up.jsx
+++ src/Pages/Sign-in-up.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { LoginForm } from '../Components/LoginForm'
 import { RegistrationForm } from '../Components/RegistrationForm'
 import '../CssFiles/Sign-in-up.css'
 
 export const SignPage = () => {
 
     return (
+        // <LoginForm />
         <RegistrationForm />
     )
 }
