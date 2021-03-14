78d24035d13920241d9d897656fb623336650781
Badm1 <shattilov@mail.ru>
2021-03-14 21:19:33 +0300

    форма авторизации

---
+++ src/Components/LoginForm.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../CssFiles/LoginForm.css'
+
+export const LoginForm = () => {
+
+
+    return (
+        <div className="login-form">
+            <form action="">
+                <div className="form-header">
+                    <h2>Вход</h2>
+                </div>
+                <div className="form-body">
+                    <input type="email" className="form-body-item" placeholder="Введите ваш E-mail" required />
+                    <input type="password" className="form-body-item" placeholder="Введите ваш пароль" required />
+                </div>
+                <div className="form-footer">
+                <Link to="/mainPage">  <button type="submit">Войти</button></Link>
+                </div>
+            </form>
+        </div>
+    )
+}
