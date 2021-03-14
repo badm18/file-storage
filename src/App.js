1360291759af30df6ee0dd462e7e88eeafc24ac6
Badm1 <shattilov@mail.ru>
2021-03-14 21:25:38 +0300

    добавлен рутинг страниц

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
 import './App.css';
+import { LoginForm } from './Components/LoginForm';
+import { RegistrationForm } from './Components/RegistrationForm';
+import { MainPage } from './Pages/MainPage';
+import { SignPage } from './Pages/Sign-in-up';
 
 function App() {
   return (
-    <h1>fdsf</h1>
+    <>
+
+      <BrowserRouter>
+        <Route exact path={"/" } component={LoginForm} />
+        <Route exact path={"/registration"} component={RegistrationForm} />
+        <Route exact path={"/mainPage"} component={MainPage} />
+      </BrowserRouter>
+
+
+
+      {/* <MainPage /> */}
+      {/* <SignPage /> */}
+    </>
+
   );
 }
 
