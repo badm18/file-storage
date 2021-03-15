d5792ccafbfc0297c97182532472f7b6a35d30fc
badm18 <shattilov@mail.ru>
2021-03-15 10:51:50 +0300

    Небольшие изменения

--- src/Components/RegistrationForm.jsx
+++ src/Components/RegistrationForm.jsx
@@ -23,15 +23,15 @@ export const RegistrationForm = () => {
                     <h2>Форма регистрации</h2>
                 </div>
                 <div className="form-body" onChange={change}>
-                    <input name="name" type="text" className="form-body-item" placeholder="Введите ваше имя"  required pattern="[A-Za-zА-Яа-я]+"
+                    <input name="name" type="text" className="form-body-item" placeholder="Введите ваше имя" required pattern="[A-Za-zА-Яа-я]+"
                         title="Должны использоваться только буквы" maxLength="20"
                     />
-                    <input name="surname" type="text" className="form-body-item" placeholder="Введите вашу фамилию"  required pattern="[A-Za-zА-Яа-я]+"
+                    <input name="surname" type="text" className="form-body-item" placeholder="Введите вашу фамилию" required pattern="[A-Za-zА-Яа-я]+"
                         title="Должны использоваться только буквы" maxLength="20"
                     />
-                    <input name="email" type="email" className="form-body-item" placeholder="Введите ваш E-mail"  required />
-                    <input name="password" type="password" className="form-body-item" placeholder="Введите ваш пароль"  required />
-                    <input name="passwordConfirm" type="password" className="form-body-item" placeholder="Введите пароль еще раз"  required />
+                    <input name="email" type="email" className="form-body-item" placeholder="Введите ваш E-mail" required />
+                    <input name="password" type="password" className="form-body-item" placeholder="Введите ваш пароль" required />
+                    <input name="passwordConfirm" type="password" className="form-body-item" placeholder="Введите пароль еще раз" required />
                 </div>
                 <div className="form-footer">
                     <Link to="/mainPage"><button type="submit">Зарегистрироваться</button></Link>
