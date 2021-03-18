abfc398c2f2a295773488f182d17cf09d6786638
badm18 <shattilov@mail.ru>
2021-03-18 17:36:05 +0300

    Небольшие изменения

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import { BrowserRouter, Redirect, Route } from 'react-router-dom';
 import './App.css';
-import { LoginForm } from './Components/LoginForm';
-import { RegistrationForm } from './Components/RegistrationForm';
+import { LoginForm } from './Components/Auth/LoginForm';
+import { RegistrationForm } from './Components/Auth/RegistrationForm';
 import { MainPage } from './Pages/MainPage';
-import { SignPage } from './Pages/Sign-in-up';
+
 
 function App() {
   return (
     <>
-
-      <BrowserRouter>
-        <Route exact path={"/" } component={LoginForm} />
-        <Route exact path={"/registration"} component={RegistrationForm} />
-        <Route exact path={"/mainPage"} component={MainPage} />
-      </BrowserRouter>
-
-
+     
+        <BrowserRouter>
+          <Route exact path={"/"} component={LoginForm} />
+          <Route exact path={"/registration"} component={RegistrationForm} />
+          <Route exact path={"/mainPage"} component={MainPage} />
+        </BrowserRouter>
 
       {/* <MainPage /> */}
       {/* <SignPage /> */}
--- src/Components/Auth/RegistrationForm.jsx
+++ src/Components/Auth/RegistrationForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import '../CssFiles/RegistrationForm.css'
+import '../../CssFiles/RegistrationForm.css'
 
 
 export const RegistrationForm = () => {
